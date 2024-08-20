1df3c727bc439fe30825b96433b1ad04f562ab9d
Jesús Hernández <jehernandez1996@gmail.com>
2024-08-20 15:22:12 +0200

    Created KiwiBTGuard and subfolders

---
+++ Assets/KiwiBehaviorTree/KiwiBTGuard.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using KiwiBehaviorTree;
+
+public class KiwiBTGuard : BehaviorTree
+{
+    public Transform[] waypoints;
+    public static float speed { get { return 2f; } }
+    public static float fovRange { get { return 6f; } }
+    public static float attackRange { get { return 1f; } }
+
+    protected override Node SetupTree()
+    {
+        Node root = new Selector(new List<Node>
+        {
+            new Sequence(new List<Node>
+            {
+                new CheckEnemyInAttackRange(this.transform),
+                new TaskAttack(this.transform),
+            }),
+            new Sequence(new List<Node>
+            {
+                new CheckEnemyInFOVRange(this.transform),
+                new TaskGoToTarget(this.transform),
+            }),
+            new TaskPatrol(this.transform, waypoints),
+        });
+
+        return root;
+    }
+}
